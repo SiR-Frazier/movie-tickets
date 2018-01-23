ee490d54671dd7341a98c45869da084d15df5c09
Bryce Frazier and Katie Fujihara <katie@>
2018-01-23 11:39:02 -0800

    add additional titles, began front-end logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,39 @@
+// backend logic
+function Ticket(title, time, age){
+  this.title = title;
+  this.time = time;
+  this.age = age;
+}
+  var matineePrice= 10
+  var nightPrice= 12
 
+Ticket.prototype.time = function () {
+    if (time==="10:00AM" || time==="4:00PM") {
+      return matineePrice
+    } else {
+      return nightPrice
+    }
+  }
+
+ Ticket.prototype.age = function (){
+    if (parseInt(this.age)<18 || parseInt(this.age)>65) {
+      return time()-=2;
+    else {
+      return time();
+    }
+  }
+
+Ticket.prototype.movie = function (){
+
+}
+
+// frontend logic
+$(document).ready(function(){
+  $("form#ticket").submit(function(event){
+    event.preventDefault();
+
+    var inputtedTitle = $("input#title").val();
+    var inputtedTime = $("input#time").val();
+    var inputtedAge = $("input#age").val();
+  });
+});
